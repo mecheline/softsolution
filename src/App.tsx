35de37a88b91fc8e4a7a8b0948a59ae48ea2514d
mecheline <ucheamaechi2016@gmail.com>
2024-11-22 15:09:06 +0100

    fix/props1

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
               flexGrow: 1,
               p: 3,
               width: { sm: `calc(100% - ${drawerWidth}px)` },
-              mt: 4,
+              mt: 3,
               backgroundColor: "#F8F9FB",
             }}
           >
